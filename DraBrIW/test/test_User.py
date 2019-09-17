a9b63113767d0c3677a130cf29f009d5b0de3038
Dragos Costache <dragos.costache@infinityworks.com>
2019-09-17 10:59:22 +0100

    Added Test -- User

---
+++ DraBrIW/test/test_User.py
@@ -0,0 +1,24 @@
+import unittest
+from DraBrIW.Brews import Americano
+
+from DraBrIW.User import User
+
+
+class TestUser(unittest.TestCase):
+    drink = Americano()
+
+    def test_init(self):
+        name = "Test Name"
+
+        test_user = User(name, TestUser.drink)
+        self.assertEqual(test_user.name, name)
+        self.assertEqual(test_user.fav_drink, TestUser.drink)
+
+    def test_badname(self):
+        name = "#¢∞§¶£$%^& ª•%^#¢ª•("
+        self.assertRaises(ValueError, User, name, TestUser.drink)
+
+    def test_baddrink(self):
+        name = "Test Name"
+        drink = "Americano"
+        self.assertRaises(ValueError, User, name, drink)
