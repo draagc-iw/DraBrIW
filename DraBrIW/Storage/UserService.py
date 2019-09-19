11f226b4ae89d63d161d7ca78b850efb610a0618
Dragos Costache <dragos.costache@infinityworks.com>
2019-09-19 10:08:05 +0100

    Added User Service

---
+++ DraBrIW/Storage/UserService.py
@@ -0,0 +1,37 @@
+from ..Utils import SingletonMeta
+import DraBrIW.User as _User
+from DraBrIW.Storage import UserDatabase
+
+from ..BaseBrew import Brew
+
+
+class UserService(metaclass=SingletonMeta):
+    def __init__(self, database: UserDatabase = None):
+        self._db: UserDatabase = database if database is not None else UserDatabase('user_db')
+
+    def add(self, user: _User.User):
+        if self._db.get(user.uid) is not None:
+            raise ValueError("User already exists")
+        self._db.add(user)
+
+    def get_with_uid(self, uid) -> _User.User:
+        return self._db.get(uid)
+
+    def get_with_name(self, name) -> list:
+        results = [user for user in self._db.get_all() if user.name == name]
+        return results
+
+    def delete(self, user: _User.User):
+        self._db.delete(user.uid)
+
+    def change_name(self, uid, new_name: str):
+        user = self._db.get(uid)
+        user.name = new_name
+        self._db.add(user)
+
+    def change_drink(self, uid, new_drink: Brew):
+        user = self._db.get(uid)
+        user.fav_drink = new_drink
+        self._db.add(user)
+
+
