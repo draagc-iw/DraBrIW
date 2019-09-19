9fdda51db900bacb366e0b3263a537804933d356
Dragos Costache <dragos.costache@infinityworks.com>
2019-09-19 10:00:59 +0100

    Database refactoring

---
+++ DraBrIW/Storage/UserDatabase.py
@@ -0,0 +1,39 @@
+from DraBrIW.Storage.Database import Database
+from DraBrIW.User import User
+from DraBrIW.Utils import TableStringFormatter
+
+class UserDatabase(Database):
+    def __init__(self, *args, **kw_args):
+        super(UserDatabase, self).__init__(*args, **kw_args)
+
+    def add(self, user: User):
+        self._db[user.uid] = user
+        self.save()
+
+    def get_all(self) -> iter:
+        return self._db.values()
+
+    def get(self, uid: int) -> User:
+        if uid in self._db.keys():
+            return self._db[uid]
+        else:
+            return None
+
+    def delete(self, uid: int):
+        if uid in self._db.keys():
+            self._db[uid] = None
+
+    def clear(self):
+        self._db = dict()
+        self.save()
+
+    def __str__(self):
+        if len(self._db) is 0:
+            return "Database empty 😢\n"
+        printer = TableStringFormatter()
+        printer.header(["PEOPLE", "DRINKS"])
+        for item in sorted(self._db.values(), key=lambda i: i.name):
+            printer.row([item.name, item.fav_drink])
+
+        final_output = printer.get() + f"\n{len(self._db.keys())} entries"
+        return final_output
